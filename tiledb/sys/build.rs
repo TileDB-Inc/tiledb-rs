33305fff13d3cda8effa6bf9e134bf362264be04
Matthew T. Perry <perrygeo@gmail.com>
2024-03-25 15:02:54 -0600

    use pkg-config to find system libtildb

--- tiledb/sys/build.rs
+++ tiledb/sys/build.rs
@@ -1,25 +1,11 @@
-use std::env;
-use std::str::FromStr;
-
-const INSTALL_ENVVAR: &str = "CMAKE_INSTALL_PREFIX";
-const INSTALL_DEFAULT: &str = "/opt/tiledb/lib";
-
 fn main() {
     // Hard coded for now
     println!("cargo:rustc-link-lib=tiledb");
-    println!(
-        "cargo:rustc-link-search=all={}",
-        match env::var(INSTALL_ENVVAR) {
-            Ok(dir) => dir,
-            Err(e) =>
-                if let env::VarError::NotPresent = e {
-                    String::from_str(INSTALL_DEFAULT).expect("&'static str")
-                } else {
-                    panic!(
-                        "Error reading environment variable '{}': {}",
-                        INSTALL_ENVVAR, e
-                    );
-                },
-        }
-    );
+
+    // Use the system's tiledb library
+    // Cargo metadata will be printed to stdout if the search was successful
+    pkg_config::Config::new()
+        .atleast_version("2.4.0")
+        .probe("tiledb")
+        .expect("Build-time TileDB library missing, version >= 2.4 not found. Try the vendored feature.");
 }
