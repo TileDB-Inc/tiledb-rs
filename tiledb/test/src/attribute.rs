a7e9e7c3550c6945fa4c3cb4905b728dd82a3619
Ryan Roelke <ryan.roelke@tiledb.com>
2024-03-25 21:51:05 -0400

    tiledb_test::attribute::arbitrary_for_datatype

--- tiledb/test/src/attribute.rs
+++ tiledb/test/src/attribute.rs
@@ -1,7 +1,9 @@
 use proptest::prelude::*;
 use tiledb::array::{Attribute, AttributeBuilder};
 use tiledb::context::Context;
-use tiledb::Result as TileDBResult;
+use tiledb::{fn_typed, Datatype, Result as TileDBResult};
+
+use crate::strategy::LifetimeBoundStrategy;
 
 pub fn arbitrary_name() -> impl Strategy<Value = String> {
     proptest::string::string_regex("[a-zA-Z0-9_]*")
@@ -12,23 +14,41 @@ pub fn arbitrary_name() -> impl Strategy<Value = String> {
         )
 }
 
+fn arbitrary_fill_value<T>() -> impl Strategy<Value = T>
+where
+    T: Arbitrary,
+{
+    proptest::prelude::any::<T>()
+}
+
+pub fn arbitrary_for_datatype(
+    context: &Context,
+    datatype: Datatype,
+) -> impl Strategy<Value = TileDBResult<Attribute<'_>>> {
+    proptest::prelude::any::<bool>()
+        .prop_flat_map(move |nullable| {
+            (Just(nullable),
+            if nullable { proptest::prelude::any::<bool>().bind() } else { Just(false).bind() })
+                .prop_flat_map(move |(nullable, fill_nullable)| {
+                    fn_typed!(arbitrary_fill_value, datatype => {
+                        (arbitrary_name(), Just(nullable), crate::filter::arbitrary_list_for_datatype(context, datatype), arbitrary_fill_value, Just(fill_nullable))
+                            .prop_map(move |(name, nullable, filters, fill, fill_nullable)| {
+                                Ok(AttributeBuilder::new(context, name.as_ref(), datatype)?
+                                    .nullability(nullable)?
+                                    .fill_value_nullability(fill, fill_nullable)?
+                                    .filter_list(&filters?)?
+                                    .build())
+                            }).bind()
+                    })
+            })
+        })
+}
+
 pub fn arbitrary(
     context: &Context,
 ) -> impl Strategy<Value = TileDBResult<Attribute>> {
-    (arbitrary_name(), crate::datatype::arbitrary_implemented()).prop_flat_map(
-        |(name, dt)| {
-            (
-                Just(name),
-                Just(dt),
-                crate::filter::arbitrary_list_for_datatype(context, dt),
-            )
-                .prop_map(|(name, dt, filters)| {
-                    Ok(AttributeBuilder::new(context, name.as_ref(), dt)?
-                        .filter_list(&filters?)?
-                        .build())
-                })
-        },
-    )
+    crate::datatype::arbitrary_implemented()
+        .prop_flat_map(|dt| arbitrary_for_datatype(context, dt))
 }
 
 #[cfg(test)]
