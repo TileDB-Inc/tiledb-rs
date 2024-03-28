858f321f9fbe074778ebb9ba6240aac5e16bb53a
Ryan Roelke <ryan.roelke@tiledb.com>
2024-03-28 16:07:37 -0400

    ContextBound in subarray.rs

--- tiledb/api/src/query/subarray.rs
+++ tiledb/api/src/query/subarray.rs
@@ -1,7 +1,7 @@
 use std::ops::Deref;
 
 use crate::array::DimensionKey;
-use crate::context::Context;
+use crate::context::{CApiInterface, Context, ContextBound};
 use crate::convert::CAPIConverter;
 use crate::query::Builder as QueryBuilder;
 use crate::Result as TileDBResult;
@@ -30,9 +30,22 @@ pub struct Subarray<'ctx> {
     context: &'ctx Context,
     raw: RawSubarray,
 }
+
+impl<'ctx> ContextBound<'ctx> for Subarray<'ctx> {
+    fn context(&self) -> &'ctx Context {
+        self.context
+    }
+}
+
 pub struct Builder<'ctx> {
     query: QueryBuilder<'ctx>,
     subarray: Subarray<'ctx>,
+}
+
+impl<'ctx> ContextBound<'ctx> for Builder<'ctx> {
+    fn context(&self) -> &'ctx Context {
+        self.subarray.context()
+    }
 }
 
 impl<'ctx> Builder<'ctx> {
@@ -42,20 +55,17 @@ impl<'ctx> Builder<'ctx> {
         let c_array = query.query.array.capi();
         let mut c_subarray: *mut ffi::tiledb_subarray_t = out_ptr!();
 
-        let c_ret = unsafe {
+        context.capi_return(unsafe {
             ffi::tiledb_subarray_alloc(c_context, c_array, &mut c_subarray)
-        };
-        if c_ret == ffi::TILEDB_OK {
-            Ok(Builder {
-                query,
-                subarray: Subarray {
-                    context,
-                    raw: RawSubarray::Owned(c_subarray),
-                },
-            })
-        } else {
-            Err(context.expect_last_error())
-        }
+        })?;
+
+        Ok(Builder {
+            query,
+            subarray: Subarray {
+                context,
+                raw: RawSubarray::Owned(c_subarray),
+            },
+        })
     }
 
     pub fn dimension_range_typed<Conv: CAPIConverter, K: Into<DimensionKey>>(
@@ -69,10 +79,10 @@ impl<'ctx> Builder<'ctx> {
         let c_start = &range[0] as *const Conv as *const std::ffi::c_void;
         let c_end = &range[1] as *const Conv as *const std::ffi::c_void;
 
-        let c_ret = match key.into() {
+        match key.into() {
             DimensionKey::Index(idx) => {
                 let c_idx = idx.try_into().unwrap();
-                unsafe {
+                self.capi_return(unsafe {
                     ffi::tiledb_subarray_add_range(
                         c_context,
                         c_subarray,
@@ -81,11 +91,11 @@ impl<'ctx> Builder<'ctx> {
                         c_end,
                         std::ptr::null(),
                     )
-                }
+                })
             }
             DimensionKey::Name(name) => {
                 let c_name = cstring!(name);
-                unsafe {
+                self.capi_return(unsafe {
                     ffi::tiledb_subarray_add_range_by_name(
                         c_context,
                         c_subarray,
@@ -94,14 +104,10 @@ impl<'ctx> Builder<'ctx> {
                         c_end,
                         std::ptr::null(),
                     )
-                }
+                })
             }
-        };
-        if c_ret == ffi::TILEDB_OK {
-            self.build()
-        } else {
-            Err(self.subarray.context.expect_last_error())
-        }
+        }?;
+        self.build()
     }
 
     fn build(mut self) -> TileDBResult<QueryBuilder<'ctx>> {
@@ -109,14 +115,10 @@ impl<'ctx> Builder<'ctx> {
         let c_query = *self.query.query.raw;
         let c_subarray = *self.subarray.raw;
 
-        let c_ret = unsafe {
+        self.capi_return(unsafe {
             ffi::tiledb_query_set_subarray_t(c_context, c_query, c_subarray)
-        };
-        if c_ret == ffi::TILEDB_OK {
-            self.query.query.subarrays.push(self.subarray);
-            Ok(self.query)
-        } else {
-            Err(self.subarray.context.expect_last_error())
-        }
+        })?;
+        self.query.query.subarrays.push(self.subarray);
+        Ok(self.query)
     }
 }
