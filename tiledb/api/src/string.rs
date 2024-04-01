8bac73b25a3670638f86fecd5c1f7382d466531b
Paul J. Davis <paul.davis@tiledb.com>
2024-04-01 12:11:29 -0500

    Initial Enumeration API wrappers

--- tiledb/api/src/string.rs
+++ tiledb/api/src/string.rs
@@ -1,24 +1,60 @@
-extern crate tiledb_sys as ffi;
+use std::ops::Deref;
 
-pub struct String {
-    _wrapped: *mut ffi::tiledb_string_t,
+use crate::error::Error;
+use crate::Result as TileDBResult;
+
+pub(crate) enum RawTDBString {
+    Owned(*mut ffi::tiledb_string_t),
 }
 
-impl Default for String {
-    fn default() -> String {
-        Self {
-            _wrapped: std::ptr::null_mut::<ffi::tiledb_string_t>(),
+impl Deref for RawTDBString {
+    type Target = *mut ffi::tiledb_string_t;
+    fn deref(&self) -> &Self::Target {
+        let RawTDBString::Owned(ref ffi) = *self;
+        ffi
+    }
+}
+
+impl Drop for RawTDBString {
+    fn drop(&mut self) {
+        let RawTDBString::Owned(ref mut ffi) = *self;
+        unsafe {
+            ffi::tiledb_string_free(ffi);
         }
     }
 }
 
-impl Drop for String {
-    fn drop(&mut self) {
-        if self._wrapped.is_null() {
-            return;
-        }
-        unsafe {
-            ffi::tiledb_string_free(&mut self._wrapped);
+pub struct TDBString {
+    pub(crate) raw: RawTDBString,
+}
+
+impl TDBString {
+    pub fn to_string(&self) -> TileDBResult<String> {
+        let mut c_str = std::ptr::null::<std::ffi::c_uchar>();
+        let mut c_len: usize = 0;
+
+        let res = unsafe {
+            ffi::tiledb_string_view(
+                *self.raw,
+                &mut c_str as *mut *const u8,
+                &mut c_len,
+            )
+        };
+
+        if res == ffi::TILEDB_OK {
+            let raw_slice: &[u8] =
+                unsafe { std::slice::from_raw_parts(c_str, c_len) };
+            let c_str = std::str::from_utf8(raw_slice).map_err(|e| {
+                Error::LibTileDB(format!(
+                    "TileDB returned a string that is not UTF-8: {}",
+                    e
+                ))
+            })?;
+            Ok(c_str.to_owned())
+        } else {
+            Err(Error::Internal(
+                "Error getting string view from core.".to_owned(),
+            ))
         }
     }
 }
