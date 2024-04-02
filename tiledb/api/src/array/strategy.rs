63de40527d1c889e155b47188388cb757c8af3dd
Ryan Roelke <ryan.roelke@tiledb.com>
2024-04-02 09:10:32 -0400

    Add Schema::load to test_array_create

--- tiledb/api/src/array/strategy.rs
+++ tiledb/api/src/array/strategy.rs
@@ -2,9 +2,11 @@
 mod tests {
     use proptest::prelude::*;
     use tempfile::TempDir;
+    use util::assert_option_subset;
+    use util::option::OptionSubset;
 
     use crate::array::schema::SchemaData;
-    use crate::array::Array;
+    use crate::array::{Array, Schema};
     use crate::context::Context;
     use crate::Factory;
 
@@ -12,18 +14,20 @@ mod tests {
     fn test_array_create() {
         let ctx = Context::new().expect("Error creating context");
 
-        proptest!(|(maybe_schema in any::<SchemaData>())| {
-            let schema = maybe_schema.create(&ctx)
+        proptest!(|(schema_spec in any::<SchemaData>())| {
+            let schema_in = schema_spec.create(&ctx)
                 .expect("Error constructing arbitrary schema");
-            assert_eq!(schema, schema);
 
-            let array_create = {
-                let tempdir = TempDir::new().expect("Error creating temp dir");
-                let uri = String::from("file:///") + tempdir.path().join("array").to_str().unwrap();
+            let tempdir = TempDir::new().expect("Error creating temp dir");
+            let uri = String::from("file:///") + tempdir.path().join("array").to_str().unwrap();
 
-                Array::create(&ctx, uri, schema)
-            };
-            array_create.expect("Error creating array from arbitrary schema");
-        });
+            Array::create(&ctx, &uri, schema_in)
+                .expect("Error creating array");
+
+            let schema_out = Schema::load(&ctx, &uri).expect("Error loading array schema");
+
+            let schema_out_spec = SchemaData::try_from(&schema_out).expect("Error creating schema spec");
+            assert_option_subset!(schema_spec, schema_out_spec);
+        })
     }
 }
