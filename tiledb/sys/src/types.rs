ce71d76c9dd17033f85f00965214be5d42d515ae
Abigale Kim <abigale.kim@tiledb.com>
2024-04-12 14:51:19 -0400

    paul's changes

--- tiledb/sys/src/types.rs
+++ tiledb/sys/src/types.rs
@@ -4,115 +4,115 @@ pub type capi_return_t = i32;
 pub type capi_status_t = i32;
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_array_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_array_schema_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_attribute_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_config_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_config_iter_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_ctx_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_dimension_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_domain_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_enumeration_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_error_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_filter_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_filter_list_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_query_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_query_condition_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_string_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_subarray_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_vfs_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_vfs_fh_t {
     _unused: [u8; 0],
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Clone, Copy, Debug)]
 pub struct tiledb_group_t {
     _unused: [u8; 0],
 }
