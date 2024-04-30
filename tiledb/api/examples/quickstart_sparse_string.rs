08eeb5fc0c112b8fdc5009c016e5bfa548a6eada
Paul J. Davis <paul.davis@tiledb.com>
2024-04-29 23:31:32 -0500

    Fix quick start sparse string example

--- tiledb/api/examples/quickstart_sparse_string.rs
+++ tiledb/api/examples/quickstart_sparse_string.rs
@@ -8,11 +8,17 @@ use tiledb::array::{
     SchemaData, TileOrder,
 };
 use tiledb::context::Context;
-use tiledb::query::{
-    Query, QueryBuilder, ReadBuilder, ReadQuery, ReadQueryBuilder, WriteBuilder,
+use tiledb::datatype::Datatype;
+use tiledb::query::buffer::{
+    AllocatedWriteBuffer, ReadBufferCollection, WriteBufferCollection,
 };
-use tiledb::Result as TileDBResult;
-use tiledb::{Datatype, Factory};
+use tiledb::query::read::{
+    ReadQueryBuilder, ReadQueryField as RQField, ReadQueryFieldAccessor,
+    ReadQueryFieldAsIterator, ReadQueryFieldAsStringIterator,
+};
+use tiledb::query::traits::QueryBuilder;
+use tiledb::query::write::WriteQueryBuilder;
+use tiledb::{Factory, Result as TileDBResult};
 
 const ARRAY_URI: &str = "quickstart_sparse_string_array";
 
@@ -24,22 +30,40 @@ fn main() -> TileDBResult<()> {
     }
 
     let array = Array::open(&ctx, ARRAY_URI, tiledb::array::Mode::Read)?;
-    let mut query = ReadBuilder::new(array)?
+    let mut query = ReadQueryBuilder::new(array)?
         .layout(tiledb::query::QueryLayout::RowMajor)?
-        .register_constructor::<_, Vec<String>>("rows", Default::default())?
-        .register_constructor::<_, Vec<i32>>("cols", Default::default())?
-        .register_constructor::<_, Vec<i32>>("a", Default::default())?
         .start_subarray()?
         .add_range("rows", &["a", "c"])?
         .add_range("cols", &[2, 4])?
         .finish_subarray()?
         .build();
 
-    let (a, (cols, (rows, ()))) = query.execute()?;
+    let rows_data = vec![0u8; 4].into_boxed_slice();
+    let rows_offsets = vec![0u64; 3].into_boxed_slice();
+    let cols_data = vec![0i32; 3].into_boxed_slice();
+    let attr_data = vec![0i32; 3].into_boxed_slice();
 
-    for (row, col, a) in izip!(rows, cols, a) {
-        println!("Cell ({} {}) has data {}", row, col, a);
+    let buffers = ReadBufferCollection::new();
+    buffers
+        .borrow_mut()
+        .add_buffer("rows", (rows_data, rows_offsets))?
+        .add_buffer("cols", cols_data)?
+        .add_buffer("attr", attr_data)?;
+
+    let result = query.submit(&buffers)?;
+    assert!(result.completed());
+
+    let slices = result.slices()?;
+    let rows: RQField<u8> = slices.field("rows")?;
+    let cols: RQField<i32> = slices.field("cols")?;
+    let attrs: RQField<i32> = slices.field("attr")?;
+
+    for (row, col, attr) in
+        izip!(rows.lossy_str_iter()?, cols.iter()?, attrs.iter()?)
+    {
+        println!("Cell ({}, {}) has attr {}", row, col, attr);
     }
+
     println!();
 
     Ok(())
@@ -65,7 +89,7 @@ fn create_array(ctx: &Context) -> TileDBResult<()> {
             ],
         },
         attributes: vec![AttributeData {
-            name: "a".to_owned(),
+            name: "attr".to_owned(),
             datatype: Datatype::Int32,
             ..Default::default()
         }],
@@ -82,20 +106,24 @@ fn create_array(ctx: &Context) -> TileDBResult<()> {
 
 fn write_array(ctx: &Context) -> TileDBResult<()> {
     let row_data = vec!["a", "bb", "c"];
+    let row_data = AllocatedWriteBuffer::from(&row_data);
     let col_data = vec![1, 4, 3];
-    let a_data = vec![1, 2, 3];
+    let attr_data = vec![1, 2, 3];
 
     let array =
         tiledb::Array::open(ctx, ARRAY_URI, tiledb::array::Mode::Write)?;
 
-    let query = WriteBuilder::new(array)?
+    let mut query = WriteQueryBuilder::new(array)?
         .layout(CellOrder::Unordered)?
-        .data_typed("rows", &row_data)?
-        .data_typed("cols", &col_data)?
-        .data_typed("a", &a_data)?
         .build();
 
-    query.submit().and_then(|_| query.finalize())?;
+    let buffers = WriteBufferCollection::new()
+        .add_buffer("rows", &row_data)?
+        .add_buffer("cols", col_data.as_slice())?
+        .add_buffer("attr", attr_data.as_slice())?;
+
+    let _ = query.submit(&buffers)?;
+    query.finalize()?;
 
     Ok(())
 }
