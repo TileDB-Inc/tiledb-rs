96015f0b1b8f974b485384a7b4b92734d56e7b9e
Ryan Roelke <ryan.roelke@tiledb.com>
2024-06-13 09:48:33 -0400

    Add SignalGuard, SignalCallback to test utilities

--- tiledb/test-utils/src/lib.rs
+++ tiledb/test-utils/src/lib.rs
@@ -1,6 +1,9 @@
 extern crate proptest;
 
+pub mod signal;
 pub mod strategy;
 pub mod uri_generators;
 
+pub use nix::sys::signal::{SigHandler, Signal};
+pub use signal::*;
 pub use uri_generators::{get_uri_generator, TestArrayUri};
