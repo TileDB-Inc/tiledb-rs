38c9c9fc0edad9a36659689618c504e62ce7309f
Ryan Roelke <ryan.roelke@tiledb.com>
2024-07-09 04:27:30 -0400

    SubarrayBuilder::dimension_ranges

--- tiledb/api/src/query/subarray.rs
+++ tiledb/api/src/query/subarray.rs
@@ -340,6 +340,24 @@ where
         Ok(self)
     }
 
+    /// Adds a set of ranges on each dimension.
+    /// The outer `Vec` of `ranges` is the list of dimensions and the inner
+    /// `Vec` is the list of requested ranges for that dimension.
+    /// If a list is empty for a dimension, then all the coordinates of that
+    /// dimension are selected.
+    pub fn dimension_ranges(
+        self,
+        ranges: Vec<Vec<Range>>,
+    ) -> TileDBResult<Self> {
+        let mut b = self;
+        for (d, ranges) in ranges.into_iter().enumerate() {
+            for r in ranges.into_iter() {
+                b = b.add_range(d, r)?;
+            }
+        }
+        Ok(b)
+    }
+
     /// Apply the subarray to the query, returning the query builder.
     pub fn finish_subarray(self) -> TileDBResult<Q> {
         let c_query = **self.query.base().cquery();
@@ -359,7 +377,7 @@ pub struct SubarrayData {
     /// The outer `Vec` is the list of dimensions and the inner `Vec`
     /// is the list of requested ranges for that dimension.
     /// If a list is empty for a dimension, then all the coordinates
-    /// are selected.
+    /// of that dimension are selected.
     pub dimension_ranges: Vec<Vec<Range>>,
 }
 
@@ -440,6 +458,7 @@ pub mod strategy {
 
 #[cfg(test)]
 mod tests {
+    use std::hash::{DefaultHasher, Hash, Hasher};
     use std::rc::Rc;
 
     use itertools::izip;
@@ -448,7 +467,10 @@ mod tests {
 
     use super::*;
     use crate::array::*;
-    use crate::query::{Query, QueryBuilder, ReadBuilder};
+    use crate::query::{
+        Query, QueryBuilder, ReadBuilder, ReadQuery, ReadQueryBuilder,
+        WriteBuilder,
+    };
     use crate::Datatype;
 
     /// The default subarray of a query with a constrained dimension
@@ -714,5 +736,113 @@ mod tests {
         fn subarray_intersect((subarray, range) in strat_subarray_intersect()) {
             do_subarray_intersect(&subarray, &range)
         }
+    }
+
+    #[test]
+    fn dimension_ranges() {
+        let ctx = Context::new().unwrap();
+
+        let test_uri = tiledb_test_utils::get_uri_generator()
+            .map_err(|e| Error::Other(e.to_string()))
+            .unwrap();
+        let test_uri = crate::array::tests::create_quickstart_sparse_string(
+            &test_uri, &ctx,
+        )
+        .unwrap();
+
+        let derive_att = |row: &str, col: &i32| -> i32 {
+            let mut h = DefaultHasher::new();
+            (row, col).hash(&mut h);
+            h.finish() as i32
+        };
+
+        // write some data
+        {
+            let row_values = vec!["foo", "bar", "baz", "quux", "gub"];
+            let col_values = (1..=4).collect::<Vec<i32>>();
+
+            let (rows, (cols, atts)) = row_values
+                .iter()
+                .flat_map(|r| {
+                    col_values.iter().map(move |c| {
+                        let att = derive_att(r, c);
+                        (r.to_string(), (*c, att))
+                    })
+                })
+                .collect::<(Vec<String>, (Vec<i32>, Vec<i32>))>();
+
+            let w = Array::open(&ctx, &test_uri, Mode::Write).unwrap();
+            let q = WriteBuilder::new(w)
+                .unwrap()
+                .data("rows", &rows)
+                .unwrap()
+                .data("cols", &cols)
+                .unwrap()
+                .data("a", &atts)
+                .unwrap()
+                .build();
+
+            q.submit().unwrap();
+            q.finalize().unwrap();
+        }
+
+        let schema = {
+            let array = Array::open(&ctx, &test_uri, Mode::Read).unwrap();
+            Rc::new(SchemaData::try_from(array.schema().unwrap()).unwrap())
+        };
+
+        let do_dimension_ranges = |subarray: SubarrayData| -> TileDBResult<()> {
+            let array = Array::open(&ctx, &test_uri, Mode::Read).unwrap();
+            let mut q = ReadBuilder::new(array)?
+                .start_subarray()?
+                .dimension_ranges(subarray.dimension_ranges.clone())?
+                .finish_subarray()?
+                .register_constructor::<_, Vec<String>>(
+                    "rows",
+                    Default::default(),
+                )?
+                .register_constructor::<_, Vec<i32>>(
+                    "cols",
+                    Default::default(),
+                )?
+                .register_constructor::<_, Vec<i32>>("a", Default::default())?
+                .build();
+
+            let (atts, (cols, (rows, _))) = q.execute()?;
+            assert_eq!(rows.len(), cols.len());
+            assert_eq!(rows.len(), atts.len());
+
+            for (row, col, att) in izip!(rows, cols, atts) {
+                assert_eq!(att, derive_att(&row, &col));
+
+                let row_in_bounds = subarray.dimension_ranges[0].is_empty()
+                    || subarray.dimension_ranges[0].iter().any(|r| {
+                        let Range::Var(VarValueRange::UInt8(ref lb, ref ub)) =
+                            r
+                        else {
+                            unreachable!()
+                        };
+                        lb.as_ref() <= row.as_bytes()
+                            && row.as_bytes() <= ub.as_ref()
+                    });
+                assert!(row_in_bounds);
+
+                let col_in_bounds = subarray.dimension_ranges[1].is_empty()
+                    || subarray.dimension_ranges[1].iter().any(|r| {
+                        let Range::Single(SingleValueRange::Int32(lb, ub)) = r
+                        else {
+                            unreachable!()
+                        };
+                        *lb <= col && col <= *ub
+                    });
+                assert!(col_in_bounds);
+            }
+
+            Ok(())
+        };
+
+        proptest!(move |(subarray in any_with::<SubarrayData>(Some(Rc::clone(&schema))))| {
+            do_dimension_ranges(subarray).expect("Read query error");
+        })
     }
 }
