236a60c277e3a259f0d6083cf9ccac89ac1eb87c
Paul J. Davis <paul.davis@tiledb.com>
2024-07-22 13:34:03 -0500

    Set local rpath lookups for libtiledb
    Adding $ORIGIN and @loader_path to the RPATH allows users to just place
    a copy of libtiledb.{so,dylib,dll} next to the binary instead of forcing
    it's presence in a specific system directory. This should allow for
    users to be able to play with the binaries created by rustc without
    having to setup a full build and install of libtiledb.

--- tiledb/sys/build.rs
+++ tiledb/sys/build.rs
@@ -1,11 +1,11 @@
 fn main() {
-    // Hard coded for now
-    println!("cargo:rustc-link-lib=tiledb");
-    let libdir = pkg_config::get_variable("tiledb", "libdir")
-        .expect("Missing tiledb dependency.");
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", libdir);
     pkg_config::Config::new()
         .atleast_version("2.20.0")
         .probe("tiledb")
         .expect("Build-time TileDB library missing, version >= 2.4 not found.");
+    println!("cargo:rustc-link-lib=tiledb");
+
+    let libdir = pkg_config::get_variable("tiledb", "libdir")
+        .expect("Missing tiledb dependency.");
+    tiledb_utils::build::set_linker_rpath(&libdir);
 }
