d17f9c6736de994a2bccf4dca5bc1e684ff8d5d4
Ryan Roelke <ryan.roelke@tiledb.com>
2024-08-27 11:27:18 -0400

    Update sys declarations after core #5225

--- tiledb/sys/src/array_type.rs
+++ tiledb/sys/src/array_type.rs
@@ -1,13 +1,14 @@
 use crate::capi_enum::tiledb_array_type_t;
+use crate::types::capi_return_t;
 
 extern "C" {
     pub fn tiledb_array_type_to_str(
         array_type: tiledb_array_type_t,
         str_: *mut *const ::std::os::raw::c_char,
-    ) -> i32;
+    ) -> capi_return_t;
 
     pub fn tiledb_array_type_from_str(
         str_: *const ::std::os::raw::c_char,
         array_type: *mut tiledb_array_type_t,
-    ) -> i32;
+    ) -> capi_return_t;
 }
