2157f1c378364dc6ba78bbcb800e41b85e3607a6
Paul J. Davis <paul.davis@tiledb.com>
2024-10-23 18:07:00 -0500

    Fixup vcpkg directory

--- tiledb/sys/build/compile.rs
+++ tiledb/sys/build/compile.rs
@@ -81,14 +81,43 @@ fn merge_libs(build_dir: &std::path::Path) -> Result<()> {
         panic!("Missing libtiled: {}", tdb.display());
     }
 
-    let mut vcpkg_libs = std::path::PathBuf::from(build_dir);
-    vcpkg_libs.extend(["vcpkg_installed", "arm64-osx", "lib"]);
-    if !vcpkg_libs.is_dir() {
-        panic!("Missing vcpkg lib directory: {}", vcpkg_libs.display());
+    let mut vcpkg_installed = std::path::PathBuf::from(build_dir);
+    vcpkg_installed.push("vcpkg_installed");
+    if !vcpkg_installed.is_dir() {
+        panic!("Missing vcpkg_installed directory.");
     }
 
-    let paths = std::fs::read_dir(vcpkg_libs)
-        .expect("Error reading vcpkg lib directory.");
+    let paths = std::fs::read_dir(vcpkg_installed)
+        .expect("Error reading vcpkg_installed");
+
+    // Filter out the `vpckg/` subdirectory and hopefully only one directory
+    // remains for us to care about.
+    let mut not_vcpkg_paths = Vec::new();
+    for path in paths.flatten() {
+        if path.path().display().to_string() == "vcpkg" {
+            continue;
+        }
+        not_vcpkg_paths.push(path.path().display().to_string());
+    }
+
+    let path = if let Some(path) = not_vcpkg_paths.first() {
+        path.to_string()
+    } else {
+        panic!("Error locating `vcpkg_installed/${{triplet}}` directory.");
+    };
+
+    let mut lib_dir = std::path::PathBuf::from(build_dir);
+    assert!(lib_dir.is_dir());
+    lib_dir.extend(["vcpkg_installed", &path, "lib"]);
+    if !lib_dir.is_dir() {
+        panic!(
+            "Missing directory vcpkg_installed/${{triplet}}/lib: {}",
+            lib_dir.display()
+        );
+    }
+
+    let paths =
+        std::fs::read_dir(lib_dir).expect("Error reading vcpkg lib directory.");
     let mut libs = vec![tdb.display().to_string()];
     for path in paths {
         if path.is_err() {
