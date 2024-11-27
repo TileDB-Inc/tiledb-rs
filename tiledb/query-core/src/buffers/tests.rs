9aaf2d50e7d79e9a907fc522a95e1ff6245594f7
Ryan Roelke <ryan.roelke@tiledb.com>
2024-11-27 16:42:01 -0500

    proptest_list_buffers_roundtrip_fixed

--- tiledb/query-core/src/buffers/tests.rs
+++ tiledb/query-core/src/buffers/tests.rs
@@ -1,4 +1,4 @@
-use arrow_proptest_strategies::prop_array;
+use arrow_proptest_strategies::array::{prop_array, ArrayParameters};
 use arrow_proptest_strategies::schema::prop_arrow_field;
 use proptest::prelude::*;
 use tiledb_common::Datatype;
@@ -90,7 +90,11 @@ fn instance_list_buffers_roundtrip_fixed(
         is_nullable: true,
     };
 
-    let lb = Box::new(ListBuffers::try_new(&target, array_in.clone()).unwrap());
+    let lb = Box::new({
+        let array_in =
+            downcast_consume::<aa::LargeListArray>(copy_array(&array_in));
+        ListBuffers::try_new(&target, array_in).unwrap()
+    });
     let array_out = match lb.into_arrow() {
         Ok(array) => array,
         Err((_, e)) => panic!("Unexpected error in `into_arrow`: {}", e),
@@ -126,9 +130,62 @@ fn strat_list_buffers_roundtrip_var(
         .prop_map(|array| downcast_consume::<aa::LargeListArray>(array))
 }
 
+fn strat_list_buffers_roundtrip_fixed(
+) -> impl Strategy<Value = (CellValNum, aa::LargeListArray)> {
+    (any::<Datatype>(), 1..=256i32)
+        .prop_filter(
+            "FIXME: Boolean in list needs special handling",
+            |(dt, _)| *dt != Datatype::Boolean,
+        )
+        .prop_map(|(dt, fl)| {
+            (
+                fl,
+                crate::datatype::default_arrow_type(dt, CellValNum::single())
+                    .unwrap()
+                    .into_inner(),
+            )
+        })
+        .prop_flat_map(|(fl, adt)| {
+            let params = ArrayParameters {
+                num_collection_elements: (fl as usize).into(),
+                ..Default::default()
+            };
+            let field = adt::Field::new(
+                "unused",
+                adt::DataType::FixedSizeList(
+                    Arc::new(adt::Field::new_list_field(adt, false)),
+                    fl,
+                ),
+                true,
+            );
+            prop_array(params, Arc::new(field))
+        })
+        .prop_map(|array| {
+            let num_lists = array.len();
+            let fl = downcast_consume::<aa::FixedSizeListArray>(dbg!(array));
+            let (field, fl, values, nulls) = fl.into_parts();
+            let array = aa::LargeListArray::try_new(
+                field,
+                abuf::OffsetBuffer::<i64>::from_lengths(vec![
+                    fl as usize;
+                    num_lists
+                ]),
+                values,
+                nulls,
+            )
+            .unwrap();
+            (CellValNum::try_from(fl as u32).unwrap(), array)
+        })
+}
+
 proptest! {
     #[test]
     fn proptest_list_buffers_roundtrip_var(array in strat_list_buffers_roundtrip_var()) {
         instance_list_buffers_roundtrip_var(array)
+    }
+
+    #[test]
+    fn proptest_list_buffers_roundtrip_fixed((cvn, array) in strat_list_buffers_roundtrip_fixed()) {
+        instance_list_buffers_roundtrip_fixed(cvn, array)
     }
 }
