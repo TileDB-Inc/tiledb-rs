234af869b4a93a2cb188bc27b99e142a9d38a6cd
Paul J. Davis <paul.davis@tiledb.com>
2025-02-24 12:43:07 -0600

    chore(ci): Fix release workflow
    As expected, I didn't get this right on the first try. This is mostly a
    few fixups plus getting a cast fro Linux on arm64 correct.

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -3,4 +3,4 @@
 NOTES=$(git cliff --unreleased --tag $1)
 git tag -a --cleanup verbatim -e -m "$NOTES" $1
 git push origin $1
-gh release create $1 --verify-tag --notes "$NOTES"
+gh release create $1 --draft --verify-tag --notes "$NOTES"
