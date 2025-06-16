dbf34c4a5c2704e874e16be9d96ecbf5790a4625
Ryan Roelke <ryan@tiledb.com>
2025-06-16 11:16:11 -0400

    Steal prop_without_replacement from tiledb-tables into strategy-ext

--- test-utils/strategy-ext/src/lib.rs
+++ test-utils/strategy-ext/src/lib.rs
@@ -3,8 +3,10 @@ pub mod meta;
 pub mod records;
 pub mod sequence;
 pub mod strategy;
+pub mod without_replacement;
 
 use std::fmt::Debug;
+use std::hash::Hash;
 
 use proptest::strategy::{Strategy, ValueTree};
 
@@ -19,6 +21,19 @@ pub trait StrategyExt: Strategy {
         Self::Tree: Clone + Debug,
     {
         meta::ValueTreeStrategy(self)
+    }
+
+    /// Returns a strategy which produces only test cases it has not previously produced.
+    /// This requires keeping the previous test cases used,
+    /// so this should not be used with large values or for too many test cases.
+    fn prop_without_replacement(
+        self,
+    ) -> without_replacement::WithoutReplacement<Self>
+    where
+        Self: Sized,
+        Self::Value: Debug + Eq + Hash,
+    {
+        without_replacement::WithoutReplacement::new(self)
     }
 
     /// Returns a strategy which produces values transformed by
