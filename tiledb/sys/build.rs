cdcf5e77901955831b111b4111d706ff380a67a6
Paul J. Davis <paul.davis@tiledb.com>
2026-01-13 13:11:45 -0600

    chore: Update to libtiledb 2.30

--- tiledb/sys/build.rs
+++ tiledb/sys/build.rs
@@ -36,10 +36,10 @@ fn configure_dynamic(libdir: String) {
 
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("2.28.0")
+        .atleast_version("2.30.0")
         .cargo_metadata(false)
         .probe("tiledb")
-        .expect("TileDB >= 2.28 not found.");
+        .expect("TileDB >= 2.30 not found.");
 
     let prefix = pkg_config::get_variable("tiledb", "prefix")
         .expect("Missing TileDB 'libdir' variable.");
