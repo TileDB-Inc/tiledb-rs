e849ba904365035894eb70490310208f5b5b4859
Theodore Tsirpanis <teo@tsirpanis.gr>
2026-02-03 19:03:33 +0200

    Simplify importing libtiledb from pkg-config. We use a fork of the pkg-config crate to skip passing `--cflags`, which we don't need, and causes private transitive dependencies to be searched.

--- tiledb/sys/build.rs
+++ tiledb/sys/build.rs
@@ -1,39 +1,4 @@
-#[cfg(target_os = "linux")]
-fn configure_rustc(_libdir: String) {
-    println!("cargo::rustc-link-lib=dylib=stdc++");
-}
-
-#[cfg(target_os = "macos")]
-fn configure_rustc(_libdir: String) {
-    println!("cargo::rustc-link-lib=dylib=c++");
-    println!("cargo::rustc-link-lib=framework=CoreFoundation");
-    println!("cargo::rustc-link-lib=framework=CoreServices");
-    println!("cargo::rustc-link-lib=framework=Security");
-    println!("cargo::rustc-link-lib=framework=SystemConfiguration");
-    println!("cargo::rustc-link-lib=framework=Network");
-}
-
-#[cfg(not(any(target_os = "linux", target_os = "macos")))]
-fn configure_rustc(_libdir: String) {
-    panic!("This operating system is not supported.")
-}
-
-fn configure_static(libdir: String) {
-    // Configure linking
-    println!("cargo::metadata=LINKAGE=static");
-    println!("cargo::rustc-link-search=native={libdir}");
-    println!("cargo::rustc-link-lib=static=tiledb_static");
-
-    // Add any extra OS specific config
-    configure_rustc(libdir);
-}
-
-fn configure_dynamic(libdir: String) {
-    println!("cargo::metadata=LINKAGE=dynamic");
-    println!("cargo::rustc-link-search=native={libdir}");
-    println!("cargo::rustc-link-lib=tiledb");
-    println!("cargo::metadata=LIBDIR={libdir}");
-}
+use std::env;
 
 fn main() {
     if std::env::var("TILEDB_SYS_DISABLE_LINKING").is_ok() {
@@ -43,26 +8,8 @@ fn main() {
 
     pkg_config::Config::new()
         .atleast_version("2.30.0")
-        .cargo_metadata(false)
+        .cargo_metadata(true)
+        .probe_cflags(env::consts::OS != "windows")
         .probe("tiledb")
         .expect("TileDB >= 2.30 not found.");
-
-    let prefix = pkg_config::get_variable("tiledb", "prefix")
-        .expect("Missing TileDB 'libdir' variable.");
-    let prefix = prefix.trim_matches('"');
-    let libdir = std::path::Path::new(prefix)
-        .join("lib")
-        .display()
-        .to_string();
-
-    // If we find a libtiledb_static.a, link statically, otherwise assume
-    // we want to link dynamically.
-    let mut path = std::path::PathBuf::from(&libdir);
-    path.push("libtiledb_static.a");
-
-    if path.exists() {
-        configure_static(libdir);
-    } else {
-        configure_dynamic(libdir);
-    }
 }
