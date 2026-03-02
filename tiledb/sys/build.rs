d9dd83e81a7023b81e9bf6aedeec48f731174f38
Theodore Tsirpanis <teo@tsirpanis.gr>
2026-03-03 01:35:56 +0200

    Use the new pkg-config option only on Windows.
    Reduces the need for downstream projects to patch their own `pkg-config` installation.

--- tiledb/sys/build.rs
+++ tiledb/sys/build.rs
@@ -33,19 +33,28 @@ fn configure_dynamic(libdir: &str) {
     println!("cargo::metadata=LIBDIR={libdir}");
 }
 
+#[cfg(windows)]
+fn maybe_set_probe_cflags(config: &mut pkg_config::Config) {
+    // TODO: Do on all platforms once this PR hits a release:
+    // https://github.com/rust-lang/pkg-config-rs/pull/183
+    // We currently do it only on Windows, to avoid downstream components patching pkg-config themselves.
+    config.probe_cflags(false);
+}
+
+#[cfg(not(windows))]
+fn maybe_set_probe_cflags(_: &mut pkg_config::Config) {}
+
 fn main() {
     if std::env::var("TILEDB_SYS_DISABLE_LINKING").is_ok() {
         println!("cargo::metadata=LINKAGE=disabled");
         return;
     }
 
-    let lib = pkg_config::Config::new()
-        .atleast_version("2.30.0")
-        .cargo_metadata(false)
-        // Not needed for our use case, and skips resolving private transitive requirements.
-        .probe_cflags(false)
-        .probe("tiledb")
-        .expect("TileDB >= 2.30 not found.");
+    let mut config = pkg_config::Config::new();
+    config.atleast_version("2.30.0").cargo_metadata(false);
+    // Not needed for our use case, and skips resolving private transitive requirements.
+    maybe_set_probe_cflags(&mut config);
+    let lib = config.probe("tiledb").expect("TileDB >= 2.30 not found.");
 
     if let Some(libdir) = lib.link_paths.first() {
         let is_static = lib.libs.iter().any(|x| x.eq("tiledb_static"));
